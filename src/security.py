526c29b022d1ca509f5560337bdcd20947502884
Pedro Hernandez <Pedro.lin.ka@gmail.com>
2020-03-19 09:02:25 -0400

    add auth

--- src/security.py
+++ src/security.py
@@ -11,10 +11,12 @@ users = [
 username_table = {user.username: user for user in users}
 userid_table = {user.id: user for user in users}
 
+
 def authenticate(username, password):
     user = username_table.get(username, None)
     if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
